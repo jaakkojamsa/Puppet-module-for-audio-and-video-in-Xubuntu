a7fe7a49482622c8c2dc39bfff7a03e439c9e523
Jaakko Jämsä <jaakko.jamsa@myy.haaga-helia.fi>
2017-11-30 23:31:15 +0200

    Audacity modified keyboard

--- avjj17/manifests/init.pp
+++ avjj17/manifests/init.pp
@@ -1,6 +1,6 @@
 class avjj17 {
 
-        File { owner => '0', group => '0', mode => '0755', }
+        File { owner => '0', group => '0', mode => '0644', }
         Package { ensure => 'installed', allowcdrom => true, }
         Service { ensure => 'running', enable => true, }
 
@@ -21,5 +21,23 @@ class avjj17 {
         package { 'musescore':}
 
         package { 'handbrake':}
+
+	file { '/tmp/audacity.cfg':
+               source => "puppet:///modules/avjj17/audacity.cfg",
+	}
+
+	file { '/bin/acconffi.sh':
+                content => template ('avjj17/acconffi.sh'),
+                owner => '0', group => '0', mode => '0755',
+	}
+
+	exec { 'acconffi.sh':
+		command => 'acconffi.sh 8600',
+		path => '/bin/',
+	}
+
+	file { '/usr/share/xfce4/backdrops/xubuntu-xenial.png':
+                source => "puppet:///modules/avjj17/taustis.png",
+	}
 
 }
---
+++ avjj17/templates/acconffi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Haetaan tarvittava polku muuttujaan
+
+cd
+POLKU=`pwd`
+
+cp /tmp/audacity.cfg $POLKU/.audacity-data/
