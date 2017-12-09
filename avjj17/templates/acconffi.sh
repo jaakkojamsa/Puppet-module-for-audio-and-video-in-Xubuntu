81e8c5d21a4e6ba12c17d86a007c125299ad7044
Your Name <you@example.com>
2017-12-09 12:58:30 +0000

    Change the acconffi.sh home directory option. Now it works.

--- avjj17/templates/acconffi.sh
+++ avjj17/templates/acconffi.sh
@@ -3,7 +3,7 @@
 # Haetaan tarvittava polku muuttujaan
 
 cd
-POLKU=`pwd`
+POLKU=`find /home/ -maxdepth 1 -mindepth 1 -type d -name "*"`
 mkdir $POLKU/.audacity-data/
 
 cp -f /tmp/audacity.cfg $POLKU/.audacity-data/
