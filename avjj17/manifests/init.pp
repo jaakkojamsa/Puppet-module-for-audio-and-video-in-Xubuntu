9c32aef2ce4077fa4e40b44f0c46abfcfc0025d1
Jaakko Jämsä <jaakko.jamsa@myy.haaga-helia.fi>
2017-12-09 18:28:46 +0200

    Next fix

--- avjj17/manifests/init.pp
+++ avjj17/manifests/init.pp
@@ -4,24 +4,24 @@ class avjj17 {
         Package { ensure => 'installed', allowcdrom => true, }
         Service { ensure => 'running', enable => true, }
 
-        file { '/tmp/koefile':
-                content => "Hello! This is the avjj17 module's test file!\n"
+	file { '/tmp/koefile':
+		content => "Hello! This is the avjj17 module's test file!\n"
         }
 
-        package { 'openssh-server':
-                ensure => "installed",
+	package { 'openssh-server':
+		ensure => "installed",
         }
 
-         file { '/etc/ssh/sshd_config':
-                content => template("avjj17/sshd_config"),
-                require => Package["openssh-server"],
-                notify => Service["ssh"],
-        }
+	file { '/etc/ssh/sshd_config':
+		content => template("avjj17/sshd_config"),
+		require => Package["openssh-server"],
+		notify => Service["ssh"],
+	}
 
         service { 'ssh':
-                ensure => 'running',
-                enable => 'true',
-                require => Package["openssh-server"],
+		ensure => 'running',
+		enable => 'true',
+		require => Package["openssh-server"],
 
                 hasstatus => 'false',
                 status => "/etc/init.d/ssh status|grep running",
@@ -40,20 +40,6 @@ class avjj17 {
         package { 'musescore':}
 
         package { 'handbrake':}
-
-	file { '/tmp/audacity.cfg':
-               source => "puppet:///modules/avjj17/audacity.cfg",
-	}
-
-	file { '/bin/acconffi.sh':
-                content => template ('avjj17/acconffi.sh'),
-                owner => '0', group => '0', mode => '0755',
-	} ->
-
-	exec { 'acconffi.sh':
-		command => 'acconffi.sh 8600',
-		path => '/bin/',
-	}
 
 	file { '/usr/share/xfce4/backdrops/xubuntu-xenial.png':
                 source => "puppet:///modules/avjj17/taustis.png",
