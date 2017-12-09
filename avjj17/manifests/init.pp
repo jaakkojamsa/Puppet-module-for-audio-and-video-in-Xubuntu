33f0cdce6cd3f4ad2982a682ccb3b6c79871552d
Jaakko Jämsä <jaakko.jamsa@myy.haaga-helia.fi>
2017-12-09 17:29:00 +0200

    Added open-ssh, Jaakko

--- avjj17/manifests/init.pp
+++ avjj17/manifests/init.pp
@@ -8,6 +8,25 @@ class avjj17 {
                 content => "Hello! This is the avjj17 module's test file!\n"
         }
 
+        package { 'openssh-server':
+                ensure => "installed",
+        }
+
+         file { '/etc/ssh/sshd_config':
+                content => template("avjj17/sshd_config"),
+                require => Package["openssh-server"],
+                notify => Service["ssh"],
+        }
+
+        service { 'ssh':
+                ensure => 'running',
+                enable => 'true',
+                require => Package["openssh-server"],
+
+                hasstatus => 'false',
+                status => "/etc/init.d/ssh status|grep running",
+        }
+
 	package { 'tree':}
 
         package { 'audacity':}
